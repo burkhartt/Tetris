687cf2358f23dc5f91f11d8a0be63f6ae9b8188b
burkhartt <timmyb_84@hotmail.com>
2013-08-13 12:49:48 -0400

    Created a well

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,11 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Tetris {
     public class Program {
-        static void Main(string[] args) {
+        private static void Main(string[] args) {
+            var well = new Well(50, 20);
+            well.GetWellMap().Draw();
+            well.ShapeCompletedDescent += (sender, eventArgs) => { };
+            Console.ReadKey();
+        }
+    }
+
+    internal class Well : IWell {
+        private readonly WellMap wellMap;
+        private IShape movingShape;
+
+        public Well(int height, int width) {
+            wellMap = new WellMap(height, width);
+        }
+
+        public void AddShape(IShape shape) {
+            movingShape = shape;
+        }
+
+        public void Continue() {
+            // other logic here
+            if (ShapeHasCompletedDescent()) {
+                ShapeCompletedDescent(this, null);
+            }
+        }
+
+        public IWellMap GetWellMap() {
+            return wellMap;
+        }
+
+        public event EventHandler ShapeCompletedDescent;
+
+        private bool ShapeHasCompletedDescent() {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class WellMap : IWellMap {
+        private readonly Cell[,] cells;
+        private readonly int height;
+        private readonly int width;
+
+        public WellMap(int height, int width) {
+            this.height = height;
+            this.width = width;
+            cells = new Cell[height,width];
+            for (var i = 0; i < height; i++) {
+                for (var j = 0; j < width; j++) {
+                    cells[i, j] = new Cell();
+                }
+            }
+            for (var i = 0; i < height; i++) {
+                cells[i, 0] = new WallCell();
+                cells[i, width - 1] = new WallCell();
+            }
+            for (var i = 0; i < width; i++) {
+                cells[height - 1, i] = new WallCell();
+            }
+        }
+
+        public void Draw() {
+            for (var i = 0; i < height; i++) {
+                for (var j = 0; j < width; j++) {
+                    cells[i, j].Draw();
+                }
+                Console.Write("\r\n");
+            }
+        }
+    }
+
+    internal class WallCell : Cell {
+        public override void Draw() {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("#");
+        }
+    }
+
+    internal class Cell {
+        public virtual void Draw() {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ");
         }
     }
 
@@ -16,6 +94,7 @@ namespace Tetris {
     }
 
     public interface IWellMap {
+        void Draw();
     }
 
     public interface IShape {
@@ -24,6 +103,5 @@ namespace Tetris {
     }
 
     public interface IShapeMap {
-
     }
 }
