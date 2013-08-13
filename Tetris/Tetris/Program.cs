3362d6e921528b6fc7de01ed6956cdb8367c65af
burkhartt <timmyb_84@hotmail.com>
2013-08-13 12:22:05 -0400

    Made some interfaces

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -4,8 +4,21 @@ using System.Linq;
 using System.Text;
 
 namespace Tetris {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
         }
+    }
+
+    public interface IWell {
+        void AddShape(IShape shape);
+    }
+
+    public interface IShape {
+        void Rotate();
+        IShapeMap GetShapeMap();
+    }
+
+    public interface IShapeMap {
+
     }
 }
